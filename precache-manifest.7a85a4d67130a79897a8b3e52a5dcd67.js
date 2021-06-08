562e37d807a393c35b36a5e07eea1a98831e48b5
Hamza <hamzaable@gmail.com>
2021-06-09 01:03:56 +0200

    Updates

---
+++ precache-manifest.7a85a4d67130a79897a8b3e52a5dcd67.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a25981b9aff0bd49c994046e69af50ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2fd0f0d50574b6f778d9",
+    "url": "/static/css/main.0798e8c6.chunk.css"
+  },
+  {
+    "revision": "80c4d9efae1e90a95f0e",
+    "url": "/static/js/2.a41fb696.chunk.js"
+  },
+  {
+    "revision": "d18dba208164d01c98054a17fb168340",
+    "url": "/static/js/2.a41fb696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fd0f0d50574b6f778d9",
+    "url": "/static/js/main.bcaafcce.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "331ec49c0d78e469c42c1d814dd45838",
+    "url": "/static/media/Avenir-Book.331ec49c.ttf"
+  },
+  {
+    "revision": "901497541657a2f24e42848bcf7fad52",
+    "url": "/static/media/Avenir-Heavy.90149754.ttf"
+  },
+  {
+    "revision": "2090551770be22b09600a40b0b4673b7",
+    "url": "/static/media/Avenir-Medium.20905517.ttf"
+  },
+  {
+    "revision": "7fc613aa32db9b0e37135cb29fae858d",
+    "url": "/static/media/Avenir-MediumOblique.7fc613aa.ttf"
+  },
+  {
+    "revision": "02df545e8ae6626054bd4b89c594be27",
+    "url": "/static/media/background-md.02df545e.svg"
+  },
+  {
+    "revision": "d5d25e5c48150f57e52f06b6f7c6513a",
+    "url": "/static/media/background-sm.d5d25e5c.svg"
+  },
+  {
+    "revision": "a88aca4da8519555b373964d8a3199b5",
+    "url": "/static/media/footer-md.a88aca4d.svg"
+  },
+  {
+    "revision": "c70b666c5e18ca598860460df8379421",
+    "url": "/static/media/footer-sm.c70b666c.svg"
+  }
+]);
